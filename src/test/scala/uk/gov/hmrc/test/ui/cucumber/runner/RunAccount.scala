f1e555c743884b993d6e3daf1b32598672772a6d
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2023-03-17 11:18:59 +0000

    ECL-296 added runner class and the scripts for ecl account

---
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/runner/RunAccount.scala
@@ -0,0 +1,21 @@
+package uk.gov.hmrc.test.ui.cucumber.runner
+
+
+
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import org.junit.runner.RunWith
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(
+  features = Array("src/test/resources/features"),
+  glue = Array("uk.gov.hmrc.test.ui.cucumber.stepdefs"),
+  plugin = Array(
+    "pretty",
+    "html:target/cucumber",
+    "json:target/cucumber.json",
+    "junit:target/test-reports/RunAccount.xml"
+  ),
+  tags = "@account"
+)
+class RunAccount {}
