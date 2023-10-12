fcc9e4d002dede45e07d15bfed831c93a5ff022a
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2023-10-12 13:57:40 +0100

    ECL-616: Update app dependencies and plugins in Acceptance Tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,14 +4,14 @@ object Dependencies {
 
   val test = Seq(
     "uk.gov.hmrc"         %% "webdriver-factory" % "0.46.0"  % Test,
-    "org.scalatest"       %% "scalatest"         % "3.2.0"   % Test,
+    "org.scalatest"       %% "scalatest"         % "3.2.17"  % Test,
     "org.scalatestplus"   %% "selenium-3-141"    % "3.2.0.0" % Test,
-    "com.vladsch.flexmark" % "flexmark-all"      % "0.35.10" % Test,
-    "io.cucumber"         %% "cucumber-scala"    % "6.9.1"   % Test,
-    "io.cucumber"          % "cucumber-junit"    % "6.9.1"   % Test,
-    "junit"                % "junit"             % "4.12"    % Test,
+    "com.vladsch.flexmark" % "flexmark-all"      % "0.64.8"  % Test,
+    "io.cucumber"         %% "cucumber-scala"    % "8.16.0"  % Test,
+    "io.cucumber"          % "cucumber-junit"    % "7.13.0"  % Test,
+    "junit"                % "junit"             % "4.13.2"  % Test,
     "com.novocode"         % "junit-interface"   % "0.11"    % Test,
-    "com.typesafe"         % "config"            % "1.3.2"   % Test
+    "com.typesafe"         % "config"            % "1.4.2"   % Test
   )
 
 }
