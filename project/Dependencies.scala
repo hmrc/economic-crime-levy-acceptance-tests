b97efeeabac2216b736aae40353968383ff1bfec
VJWatson <171040641+VJWatson@users.noreply.github.com>
2025-06-27 10:06:04 +0100

    app deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "ui-test-runner"    % "0.45.0"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"    % "0.46.0"   % Test,
     "org.scalatest"       %% "scalatest"         % "3.2.19"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"     % "3.2.17.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all"      % "0.64.8"   % Test,
-    "io.cucumber"         %% "cucumber-scala"    % "8.25.1"   % Test,
-    "io.cucumber"          % "cucumber-junit"    % "7.20.1"   % Test,
+    "io.cucumber"         %% "cucumber-scala"    % "8.28.0"   % Test,
+    "io.cucumber"          % "cucumber-junit"    % "7.23.0"   % Test,
     "junit"                % "junit"             % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface"   % "0.11"     % Test,
     "com.typesafe"         % "config"            % "1.4.3"    % Test
