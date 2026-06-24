300ef071c6d4d214b498c88ee561eacb662233c5
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2026-06-24 10:58:24 +0100

    Platform Upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,15 +1,15 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "ui-test-runner"    % "0.50.0"   % Test,
-    "org.scalatest"       %% "scalatest"         % "3.2.19"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"    % "0.54.0"   % Test,
+    "org.scalatest"       %% "scalatest"         % "3.2.20"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"     % "3.2.17.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all"      % "0.64.8"   % Test,
     "junit"                % "junit"             % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface"   % "0.11"     % Test,
-    "com.typesafe"         % "config"            % "1.4.6"    % Test
+    "com.typesafe"         % "config"            % "1.4.9"    % Test
   )
 
 }
