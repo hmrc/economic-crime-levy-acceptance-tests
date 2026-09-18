347d2db2231497c1aa2a6c5a4ff6ecb533b7e12b
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-09-18 10:55:09 +0100

    updating test runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "ui-test-runner"    % "0.54.0"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"    % "0.55.0"   % Test,
     "org.scalatest"       %% "scalatest"         % "3.2.20"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"     % "3.2.17.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all"      % "0.64.8"   % Test,
